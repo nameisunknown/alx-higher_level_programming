7deb97212fe8a17ec86f1699a6d255e692892769
nameisunknown <orangekapital@gmail.com>
2024-01-03 16:34:34 -0800

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for count in range(0, 100):
-    if count < 10:
-        print("0{}, ".format(count), end="")
-    elif count < 99:
-        print("{}, ".format(count), end="")
+    if count < 99:
+        print("{:02}, ".format(count), end="")
     elif count == 99:
         print("{}".format(count), end="\n")
