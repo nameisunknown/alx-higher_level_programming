3c6153aaef4a1ecc574c8e7c3a5391c70ddb4d69
nameisunknown <orangekapital@gmail.com>
2024-01-05 01:22:41 -0800

    How to Make a Script Dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,13 @@ if __name__ == "__main__":
     import sys
     cmdsys = sys.argv
     count = 1
-    while count < len(cmdsys):
-        print("{}: {}".format(count, sys.argv[count]))
-        count = count + 1
+    if (len(cmdsys) == 1):
+        print("0 arguments.")
+    elif (len(cmdsys) == 2):
+        print("1 argument:", end="\n")
+        print("1: {}".format(cmdsys[1])) 
+    else:
+        while count < len(cmdsys):
+            print("{} arguments:".format(len(cmdsys)))
+            print("{}: {}".format(count, cmdsys[count]))
+            count = count + 1
