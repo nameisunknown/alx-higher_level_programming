b7d32834ca7db15c985089a32fc96d2020f9ed57
olawalesegun <olawalesegun0@gmail.com>
2024-01-10 02:34:03 +0100

    Advanced: 14. CPython #0: Python lists. CPython is the reference implementation of the Python programming language. Written in C, CPython is the default and most widely used implementation of the language.

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - Prints some basic info about Python lists.
+ * @p: Is the list to print its info
+ * Return: void
+*/
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size = PyList_Size(p);
+	Py_ssize_t i;
+	PyObject *item;
+
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
