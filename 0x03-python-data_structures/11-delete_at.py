7c3237da1f503bf6669be0088eb8935c4a6042e1
olawalesegun <olawalesegun0@gmail.com>
2024-01-10 02:44:45 +0100

    Quick Fix: optimizing the code

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        count = 0
-        newl = []
-        while count < len(my_list):
-            if count != idx:
-                newl.append(my_list[count])
-            count += 1
         del my_list[idx]
-        return newl
+        return my_list
