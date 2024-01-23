fc6aac7517b7b5241f042b8d0aa23a91c75a7d84
nameisunknown <orangekapital@gmail.com>
2024-01-23 16:02:19 +0100

    Fix: a function that divides 2 integers and prints the result.:: fixing error on why its not checking

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,10 +3,7 @@
 def safe_print_division(a, b):
     result = None
     try:
-        if a > b:
-            result = a / b
-        elif b > a:
-            result = b / a
+        result = a / b
     except (ValueError, TypeError, ZeroDivisionError):
         pass
     finally:
