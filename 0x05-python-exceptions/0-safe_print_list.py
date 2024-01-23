bc61f9a8c4a1c42e25ed184bce2ea7ed5d8765a0
nameisunknown <orangekapital@gmail.com>
2024-01-23 13:20:07 +0100

    Fix: fixed code to meet all requirements and meet pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -21,6 +21,6 @@ def safe_print_list(my_list=[], x=0):
                 count = count + 1
             print()
             return count
-    except(IndexError, TypeError):
+    except (IndexError, TypeError):
         pass
     return len_of_list
