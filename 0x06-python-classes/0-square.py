e3cfed0cdc127a6a10d33dd3fe88736a4e2a909b
nameisunknown <orangekapital@gmail.com>
2024-01-24 03:54:41 +0100

    Fix: writing an empty class Square that defines a square :: updating to ensure checker checks by wring python program as wanted in the task given

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 
+"""
+    This module details all that we need to get started
+    with classes and objects in python. Here we starting 
+    by just cretaing an empty class.
+"""
+
+
 class Square:
-    """This Is an empty class"""
+    """Nothing here, just an empty class"""
     pass
