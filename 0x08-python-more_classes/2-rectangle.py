b25679caf06d9a1c595645f291b9e9a3abb14318
nameisunknown <orangekapital@gmail.com>
2024-02-07 16:40:18 +0100

    fix: a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)
    Private instance attribute: width:
    property def width(self): to retrieve it
    property setter def width(self, value): to set it:
    width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
    if width is less than 0, raise a ValueError exception with the message width must be >= 0
    Private instance attribute: height:
    property def height(self): to retrieve it
    property setter def height(self, value): to set it:
    height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
    if height is less than 0, raise a ValueError exception with the message height must be >= 0
    Instantiation with optional width and height: def __init__(self, width=0, height=0):
    Public instance method: def area(self): that returns the rectangle area
    Public instance method: def perimeter(self): that returns the rectangle perimeter:
    if width or height is equal to 0, perimeter is equal to 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,6 +7,16 @@ class Rectangle:
     """This is a Rectangle class"""
 
     def __init__(self, width=0, height=0):
+        """
+        This method initializez the instance of Rectangle class
+
+        Args:
+            width (int): takes the width of the instance upon init
+            height (int): takes the height of the instance upon init
+        Raises:
+            TypeError: if value entered isn't integer
+            ValueError: if value entered is less than 0
+        """
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
@@ -21,10 +31,25 @@ class Rectangle:
 
     @property
     def width(self):
+        """
+        This method acts as the getter method
+
+        Return: the value of width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        This method sets the value of width
+
+        Args:
+            value (int): this value is assigned to the width
+
+        Raises:
+            TypeError: if value entered is not of type int
+            ValueError: if value is < 0
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -33,10 +58,26 @@ class Rectangle:
 
     @property
     def height(self):
+        """
+        This method acts as the getter method
+
+        Return: the value of heigth
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        This method sets the value of heigth
+
+        Args:
+            value (int): this value is assigned to the heigth
+
+        Raises:
+            TypeError: if value entered is not of type int
+            ValueError: if value is < 0
+        """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -44,10 +85,21 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """
+        This method checks for the area of the Rectangle
+
+        Return: the area of rectangle
+        """
         return self.__height * self.__width
 
     def perimeter(self):
-        if self.__width == 0 or self.__width == 0:
+        """
+        This method returns the perimeter of the Rectangle
+
+        Return:
+            if width or height = 0, return 0 and if otherwise, perimeter
+        """
+        if self.__width == 0 or self.__heigth == 0:
             return 0
         else:
             return 2 * (self.__height + self.__width)
