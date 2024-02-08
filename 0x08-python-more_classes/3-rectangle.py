ec96e84b6f94af1d43cf49fb945ace5672e41d27
nameisunknown <orangekapital@gmail.com>
2024-02-08 22:13:53 +0100

    a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)
    Private instance attribute: width:
    property def width(self): to retrieve it
    property setter def width(self, value): to set it:
    width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
    if width is less than 0, raise a ValueError exception with the message width must be >= 0
    Private instance attribute: height:
    property def height(self): to retrieve it
    property setter def height(self, value): to set it:
    height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
    if height is less than 0, raise a ValueError exception with the message height must be >= 0
    Instantiation with optional width and height: def __init__(self, width=0, height=0):
    Public instance method: def area(self): that returns the rectangle area
    Public instance method: def perimeter(self): that returns the rectangle perimeter:
    if width or height is equal to 0, perimeter has to be equal to 0
    print() and str() should print the rectangle with the character #: (see example below)
    if width or height is equal to 0, return an empty string

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+
+"""This is a class that focuses on Rectangle and the str method"""
+
+
 class Rectangle:
     """
     This class defines a ractangnd focuses on string representation
