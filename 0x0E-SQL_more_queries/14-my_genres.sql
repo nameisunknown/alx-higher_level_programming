9187a2b91cac8294ddf8391826b81e3427286af0
nameisunknown <orangekapital@gmail.com>
2024-03-14 18:26:09 -0700

    fix: --0x0E-SQL_more_queries

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,16 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server:
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+
+-- This script uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_genres.name FROM tv_genres JOIN tv_show_genres JOIN tv_shows
+ON tv_genres.id = tv_show_genres.genre_id
+and tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,8 @@
+-- This script creates the table id_not_null on your MySQL server.
+
+-- id_not_null description:
+-- id INT with the default value 1
+-- name VARCHAR(256)
+-- If the table id_not_null already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS id_not_null(id INT NOT NULL DEFAULT 1, name VARCHAR(256));
