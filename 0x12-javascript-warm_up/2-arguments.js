b5ce6a96d2e31b13dc54e76909572205acaae6a4
nameisunknown <orangekapital@gmail.com>
2024-04-11 17:14:49 -0700

    Write a script that prints a message depending of the number of arguments passed:
    If no arguments are passed to the script, print “No argument”
    If only one argument is passed to the script, print “Argument found”
    Otherwise, print “Arguments found”
    You must use console.log(...) to print all output
    You are not allowed to use var
    Reference: process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,10 @@
 const { argv } = require('node:process');
 const len = argv.length;
 
-if (len === 3) {
-  console.log('Argument found');
-} else if (len > 3) {
-  console.log('Arguments found');
-} else {
+if (len === 2) {
   console.log('No Argument');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
